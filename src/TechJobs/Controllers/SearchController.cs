18be50ef620fd09eb8d616f3418ed06eccbcd6dc
Shay Throgmartin <throgmartin@hotmail.com>
2017-03-22 20:21:01 -0500

    Built fully according to assignment specs

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using TechJobs.Models;
+using System.Linq;
 
 namespace TechJobs.Controllers
 {
@@ -10,6 +11,37 @@ namespace TechJobs.Controllers
         {
             ViewBag.columns = ListController.columnChoices;
             ViewBag.title = "Search";
+            
+            return View();
+        }
+
+        public IActionResult Results()
+        {
+            var jobs = new List<Dictionary<string, string>>();
+            var searchTerm = Request.Query["searchTerm"].ToString();
+            var searchType = Request.Query["searchType"].ToString();
+            ViewBag.searchTerm = searchTerm;
+
+            foreach (var job in JobData.FindAll())
+            {
+                if (searchType == "all")
+                {
+                    if (job.Values.Any(value => value.ToUpper().Contains(searchTerm.ToUpper())))
+                    {
+                        jobs.Add(job);
+                    }
+                }
+                else
+                {
+                    if (job.ContainsKey(searchType) && job[searchType].ToUpper().Contains(searchTerm.ToUpper()))
+                    {
+                        jobs.Add(job);
+                    }
+                }
+            }
+
+            ViewBag.jobs = jobs;
+                        
             return View();
         }
 
---
+++ src/TechJobs/Models/ResultsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobs.Models
+{
+    public class ResultsModel
+    {
+        public string Name;
+        public string Message;
+
+    }
+}
